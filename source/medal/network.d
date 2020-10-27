f1ef58cdf906313a71ed59bbe06647e11f14e1b0
Tomoya Tanjo <ttanjo@gmail.com>
2020-10-27 04:31:50 +0000

    Add medal/engine.d

--- source/medal/network.d
+++ source/medal/network.d
@@ -1,6 +1,7 @@
 module medal.network;
 
 import medal.transition;
+import medal.engine;
 
 import std;
 
@@ -43,13 +44,8 @@ immutable class PortTransition_: Transition
 
     override void fire(in BindingElement be, Tid networkTid) const
     {
-        send(networkTid, PortSent(be));
+        send(networkTid, EngineWillStop(be));
     }
-}
-
-struct PortSent
-{
-    BindingElement bindingElement;
 }
 
 unittest
@@ -75,119 +71,4 @@ unittest
         },
         (Variant _) { assert(false); },
     );
-}
-
-///
-class Engine
-{
-    ///
-    this(in Transition[] trs)
-    in(!trs.empty)
-    do
-    {
-        transitions = trs;
-        store = new Store(trs);
-        rule = new Rule(trs);
-    }
-
-    ///
-    void run(in BindingElement initBe, Tid networkTid)
-    {
-        auto running = true;
-        send(thisTid, cast(immutable)initBe);
-        while(running)
-        {
-            receive(
-                (in BindingElement be) {
-                    store.put(be);
-                    foreach(tr; rule.transitions)
-                    {
-                        if (auto nextBe = cast(immutable)tr.fireable(store))
-                        {
-                            store.remove(nextBe);
-                            spawn((in Transition t, immutable BindingElement be) => t.fire(be, ownerTid),
-                                  tr, nextBe);
-                        }
-                    }
-                },
-                (in SignalSent sig) {
-                    // send sig to all transitions
-                    running = false;
-                },
-                (in PortSent p) {
-                    // send sig? to all transitions
-                    send(networkTid, p.bindingElement);
-                    running = false;
-                },
-                (Variant v) {
-                    running = false;
-                },
-            );
-        }
-    }
-
-    Transition[] transitions;
-    Store store;
-    Rule rule;
-}
-
-class Store
-{
-    this(in Transition[] trs) pure
-    {
-    }
-
-    ///
-    void put(in BindingElement be) pure
-    //in(be.tokenElements.byPair.all!(pt => pt[0] in state))
-    do
-    {
-        foreach(p, t; be.tokenElements)
-        {
-            auto tokens = state.get(p, []);
-            state[p] = tokens ~ t;
-        }
-    }
-
-    ///
-    void remove(in BindingElement be)
-    in(be.tokenElements.byPair.all!(pt => pt[0] in state))
-    in(be.tokenElements.byPair.all!(pt => state[pt[0]].canFind(pt[1])))
-    do
-    {
-        foreach(p, t; be.tokenElements)
-        {
-            auto tokens = p in state;
-            auto split = (*tokens)[].findSplit([t]);
-            state[p] = split[0] ~ split[2];
-        }
-    }
-
-    // `dispatch(BindingElement) const pure` needs much duplication cost...
-
-    override string toString() const pure
-    {
-        return state.to!string;
-    }
-    const(Token)[][Place] state;
-}
-
-class Rule
-{
-    this(in Transition[] trs) pure
-    {
-        transitions = trs;
-    }
-
-    /+
-    auto dispatch(Store store, BindingElement trigger) const pure
-    {
-        return transitions.map!(t => t.fireable(store))
-                          .filter!(tuple => !tuple.isNull)
-                          .map!(tuple => tuple.get)
-                          .array;
-        // return tuple(Transition, BindingElement)[]
-    }+/
-
-    Transition[] transitions;
 }
