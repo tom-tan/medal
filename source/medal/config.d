a3d8044cb89288953ab84d86758f9f96e1568cbe
Tomoya Tanjo <ttanjo@gmail.com>
2020-11-16 05:25:57 +0000

    Improve config inheritance

--- source/medal/config.d
+++ source/medal/config.d
@@ -31,15 +31,25 @@ module medal.config;
     ///
     Config inherits(Config parent, bool inheritReuse = false) inout pure
     {
-        import std.algorithm : either;
+        import std.algorithm : canFind, either;
         import std.array : replace;
+        import std.functional : not;
+        import std.range : empty;
         
-        auto t = either(tag.replace("~(tag)", parent.tag), parent.tag);
-        auto wdir = either(workdir.replace("~(workdir)", parent.workdir),
-                           parent.workdir);
-        auto tdir = either(tmpdir.replace("~(tmpdir)", parent.tmpdir)
-                                 .replace("~(workdir)", parent.workdir),
-                           parent.tmpdir);
+        string t;
+        if (tag.canFind("~(tag)"))
+        {
+            t = tag.replace("~(tag)", parent.tag);
+        }
+        else
+        {
+            t = either!(not!empty)(parent.tag, tag);
+        }
+        auto wdir = either!(not!empty)(workdir.replace("~(workdir)", parent.workdir),
+                                       parent.workdir);
+        auto tdir = either!(not!empty)(tmpdir.replace("~(tmpdir)", parent.tmpdir)
+                                             .replace("~(workdir)", parent.workdir),
+                                       parent.tmpdir);
         auto leaveDir = parent.leaveTmpdir;
         auto reuse = inheritReuse ? parent.reuseParentTmpdir
                                   : tdir == parent.tmpdir;
@@ -48,6 +58,34 @@ module medal.config;
             leaveTmpdir: leaveDir, reuseParentTmpdir: reuse,
         };
         return c;
+    }
+
+    unittest
+    {
+        Config p = { tag: "" };
+        Config c = { tag: "child" };
+        assert(c.inherits(p).tag == "child");
+    }
+
+    unittest
+    {
+        Config p = { tag: "parent" };
+        Config c = { tag: "" };
+        assert(c.inherits(p).tag == "parent");
+    }
+
+    unittest
+    {
+        Config p = { tag: "foo" };
+        Config c = { tag: "~(tag).bar" };
+        assert(c.inherits(p).tag == "foo.bar");
+    }
+
+    unittest
+    {
+        Config p = { tag: "parent" };
+        Config c = { tag: "child" };
+        assert(c.inherits(p).tag == "parent");
     }
 }
 
