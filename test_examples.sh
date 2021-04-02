5d848167fd19fde141f80794921fee6b33784ae5
Tomoya Tanjo <ttanjo@gmail.com>
2021-04-02 04:47:51 +0000

    Update test_example.sh

--- test_examples.sh
+++ test_examples.sh
@@ -6,11 +6,14 @@ code=0
 
 for ex in examples/*
 do
-    ./bin/medal $ex/network.yml -i $ex/init.yml --workdir=$ex --quiet > /dev/null
+    dir=$(head -c10 /dev/urandom | base64 | tr -dc 'a-zA-Z0-9' | cut -c 1-10)
+    mkdir -p $dir
+    ./bin/medal $ex/network.yml -i $ex/init.yml --workdir=$ex --tmpdir=$dir/tmp --leave-tmpdir --log=$dir/medal.json > /dev/null
     if [ $? -eq 0 ]; then
         echo "success: $ex"
+        rm -rf $dir
     else
-        echo "failed: $ex"
+        echo "failed: $ex (see: $dir)"
         code=1
     fi
 done
